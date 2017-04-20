f9c09c28b4e8bd868eef7751222de044f9fb72dc
Thomas Lucas <tom@thomaslucas.co.uk>
2017-04-20 15:12:05 +0100

    Initial commit. Got basic data fetching structure

---
+++ jstore.php
@@ -0,0 +1,41 @@
+<?php
+
+class jstore
+{
+    private $json = '';
+    public $datapath = '';
+    public function __construct($saveto = '')
+    {	
+        if($saveto == ''){ die('Must set storage destination!'); }
+        // If storage destination doesn't exist, create it
+        if(!file_exists($saveto.'/schemas')){
+            mkdir($saveto.'/schemas', 0777, True);
+        }
+        $this->datapath = $saveto;
+    }
+
+    // Obtain a list of keys (JSON files) stored in datapath
+    public function getSets() {
+        $setlist = [];
+        foreach( glob($this->datapath."/*.json") as $file){
+        preg_match("`".$this->datapath."/(.*).json`", $file, $key, PREG_OFFSET_CAPTURE);
+        $setlist[] = $key[1][0];
+        }
+        return $setlist;
+    }
+
+    public function get($key) {
+        $this->json = file_get_contents($this->datapath.'/'.$key.'.json');
+        return $this;
+    }
+
+    public function toJSON() {
+        return $this->json;
+    }
+
+    public function toArray(){
+        $array = json_decode($this->json, TRUE);
+        return $array;
+    }
+
+}
