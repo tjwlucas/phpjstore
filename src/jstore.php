01be843890f5236dd13e2b0b7031b8a7e3421098
Thomas Lucas <tom@thomaslucas.co.uk>
2017-04-21 15:44:36 +0100

    Moved jstoreObject class into separate file to stick to one class per file standard

--- src/jstore.php
+++ src/jstore.php
@@ -175,45 +175,4 @@ class jstore
     }
 }
 
-class jstoreObject
-{
-    public $json;
-    public $key;
-    public function __construct($store)
-    {
-        $this->store = $store;
-    }
-    public function save()
-    {
-        return file_put_contents($this->store->datapath."/data/$this->key.json", $this->json);
-    }
-    
-    public function toJSON()
-    {
-        return $this->json;
-    }
 
-    public function toArray()
-    {
-        $array = json_decode($this->json, true);
-        return $array;
-    }
-
-    public function set($newvalues)
-    {
-        $array = $this->toArray();
-        foreach ($newvalues as $key => $value) {
-            $array[$key] = $value;
-        }
-        $this->json = json_encode($array, JSON_PRETTY_PRINT);
-        return $this;
-    }
-
-    public function delete($key)
-    {
-        $array = $this->toArray();
-        unset($array[$key]);
-        $this->json = json_encode($array, JSON_PRETTY_PRINT);
-        return $this;
-    }
-}
